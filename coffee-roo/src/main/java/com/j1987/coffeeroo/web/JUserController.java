0a9eafd4f92bb2a84359691831bf0582772bb281
Depich1987 <agahfranckjanel@yahoo.fr>
2014-05-19 19:54:11 +0000

    #3 Basic reporting configuration using jasperreport
    1.add dependencies to the POM.xml file (maven)
    2.add jasper-views.xml
    3.add jrxml file related
    3.update any existing files

--- coffee-roo/src/main/java/com/j1987/coffeeroo/web/JUserController.java
+++ coffee-roo/src/main/java/com/j1987/coffeeroo/web/JUserController.java
@@ -3,9 +3,11 @@ import com.j1987.coffeeroo.domain.JUser;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.gvnix.web.report.roo.addon.GvNIXReports;
 
 @RequestMapping("/jusers")
 @Controller
 @RooWebScaffold(path = "jusers", formBackingObject = JUser.class)
+@GvNIXReports({ "userreportlist|pdf" })
 public class JUserController {
 }
